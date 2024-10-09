57b494ad2b39f4492c85dbb2d3f1896d87935c34
Jalen Ashanti <jalenmshaw@gmail.com>
2024-10-09 16:26:29 -0400

    Domain Layer: Updated constructor for Category class

--- FlipBuddyWebApplication.Domain/Models/Category.cs
+++ FlipBuddyWebApplication.Domain/Models/Category.cs
@@ -2,7 +2,14 @@
 {
     public class Category
     {
-        public Category() { }
+        public Category(int categoryId,
+                        string name,
+                        string bio) 
+        {
+            CategoryId = categoryId;
+            Name = name;
+            Bio = bio;
+        }
 
         public int CategoryId { get; set; }
         public string Name { get; set; } = string.Empty;
