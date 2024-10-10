7f7855faead7f1b9a19821bd7bc54d6e63b3a2c8
Jalen Ashanti <jalenmshaw@gmail.com>
2024-10-10 17:55:02 -0400

    Domain Layer: Updated User model and constructor

--- FlipBuddyWebApplication.Domain/Models/User.cs
+++ FlipBuddyWebApplication.Domain/Models/User.cs
@@ -2,6 +2,21 @@
 {
 	public class User
 	{
+		public User(string username,
+					string firstName,
+					string lastName,
+					string password,
+					string email,
+					int subscriptionTier) 
+		{
+			Username = username;
+			FirstName = firstName;
+			LastName = lastName;
+			Password = password;
+			Email = email;
+			SubscriptionTier = subscriptionTier;
+		}	
+
 		public string Username { get; set; } = string.Empty;
 		public string FirstName {  get; set; } = string.Empty;
 		public string LastName { get; set; } = string.Empty;
