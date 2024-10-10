990191d1c8b7dcf2ba27b5459c3ec53a865c9c57
Jalen Ashanti <jalenmshaw@gmail.com>
2024-10-10 17:57:02 -0400

    Persistence Layer: Created RepositoryBase class for repositories to inheret

---
+++ FlipBuddyWebApplication.Persistence/Abstractions/RepositoryBase.cs
@@ -0,0 +1,14 @@
+﻿using FlipBuddyWebApplication.Persistence.API;
+using FlipBuddyWebApplication.Persistence.Factories;
+
+namespace FlipBuddyWebApplication.Persistence.Abstractions
+{
+    public abstract class RepositoryBase
+    {
+        protected readonly ExternalApiService _externalApiService;
+        public RepositoryBase(ExternalApiService externalApiService)
+        {
+            _externalApiService = externalApiService;
+        }
+    }
+}
