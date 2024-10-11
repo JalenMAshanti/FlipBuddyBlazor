3d68d0be2832acd2dd110a8981e389b2a39e2702
Jalen Ashanti <jalenmshaw@gmail.com>
2024-10-11 01:53:45 -0400

    Persistence Layer: Fixed Product Repository GetProductsByUserGuid Method

--- FlipBuddyWebApplication.Persistence/Repositories/ProductRepository.cs
+++ FlipBuddyWebApplication.Persistence/Repositories/ProductRepository.cs
@@ -1,7 +1,9 @@
 ﻿using FlipBuddyWebApplication.Domain.Models;
 using FlipBuddyWebApplication.Persistence.Abstractions;
 using FlipBuddyWebApplication.Persistence.API;
+using FlipBuddyWebApplication.Persistence.API.ApiResponses.CategoryResponses;
 using FlipBuddyWebApplication.Persistence.API.ApiResponses.ProductResponses;
+using System;
 
 namespace FlipBuddyWebApplication.Persistence.Repositories
 {
@@ -11,8 +13,15 @@ namespace FlipBuddyWebApplication.Persistence.Repositories
         {
         }
 
-        public async Task<List<Product>> GetProductsByUserGuid(string guid) => (await _externalApiService.GetListAPIResponse<GetProductsByUserGuidResponse>($"https://localhost:7294/Product/GetProductsByUserGuid?UserGuid={guid}")).Select(_ => _.AsDomainProduct()).ToList();
+        public async Task<List<Product>> GetProductsByUserGuid(string guid) 
+        { 
+            var response = (await _externalApiService.GetAPIResponse<GetProductsByUserGuidResponse>($"https://localhost:7294/Product/GetProductsByUserGuid?UserGuid={guid}"));
+            ArgumentNullException.ThrowIfNull(response.products);
+			var result = response.products.Select(_ => _.AsDomainProduct()).ToList();
+			return result;
+        }
 
+       
         public async Task<HttpResponseMessage> InsertProduct(object request) => await _externalApiService.PostAPIRequest("https://localhost:7294/Product/InsertProduct", request);
     }
 }
