1b50609c66e75de641aa5f87dbbd32d5bd5184ce
Jalen Ashanti <jalenmshaw@gmail.com>
2024-10-11 01:55:22 -0400

    Persistence Layer: Updated API response Model for GetProductsByUserGuid

--- FlipBuddyWebApplication.Persistence/API/ApiResponses/ProductResponses/GetProductsByUserGuidResponse.cs
+++ FlipBuddyWebApplication.Persistence/API/ApiResponses/ProductResponses/GetProductsByUserGuidResponse.cs
@@ -1,9 +1,30 @@
 ﻿using FlipBuddyWebApplication.Domain.Models;
+using System.Globalization;
 
 namespace FlipBuddyWebApplication.Persistence.API.ApiResponses.ProductResponses
 {
+    public class ProductResponse
+    {
+        public string guid { get; set; } = string.Empty;
+        public string? userGuid { get; set; } = string.Empty;
+        public string title { get; set; }= string.Empty;
+        public int categoryId { get; set; }
+        public int purchasedPrice { get; set; }
+        public int sellPrice { get; set; }
+        public string description { get; set; } = string.Empty;
+        public int quantity { get; set; }
+        public string currency { get; set; } = string.Empty;
+        public int conditionId { get; set; }
+        public string barCode { get; set; } = string.Empty;
+
+        public Product AsDomainProduct() => new(title, categoryId, purchasedPrice, sellPrice, description, quantity, currency, conditionId, barCode, "");
+    }
+
     public class GetProductsByUserGuidResponse
     {
-        public Product AsDomainProduct() => throw new NotImplementedException();
+        public List<ProductResponse>? products { get; set; }
     }
+
+
+
 }
