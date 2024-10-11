39f200bfa9c34f33db202607744c0175e94e503a
Jalen Ashanti <jalenmshaw@gmail.com>
2024-10-11 01:54:39 -0400

    Domain Layer: Changed DateSold property from DateOnly to String

--- FlipBuddyWebApplication.Domain/Models/Product.cs
+++ FlipBuddyWebApplication.Domain/Models/Product.cs
@@ -1,41 +1,39 @@
-﻿using System.Reflection;
-
-namespace FlipBuddyWebApplication.Domain.Models
+﻿namespace FlipBuddyWebApplication.Domain.Models
 {
-	public class Product
-	{
-		public Product(string title,
-						int categoryId,
-						decimal puchasePrice,
-						decimal sellPrice,
-						string description,
-						int quantity,
-						string currency,
-						int conditionId,
-						int barcode,
-						DateOnly dateSold) 
-		{
-			Title = title;
-			CategoryId = categoryId;
-			PurchasePrice = puchasePrice;
-			SellPrice = sellPrice;
-			Description = description;
-			Quantity = quantity;
-			Currency = currency;
-			ConditionId = conditionId;
-			Barcode = barcode;
-			DateSold = dateSold;
-		}
+    public class Product
+    {
+        public Product(string title,
+                        int categoryId,
+                        decimal puchasePrice,
+                        decimal sellPrice,
+                        string description,
+                        int quantity,
+                        string currency,
+                        int conditionId,
+                        string barcode,
+                        string dateSold)
+        {
+            Title = title;
+            CategoryId = categoryId;
+            PurchasePrice = puchasePrice;
+            SellPrice = sellPrice;
+            Description = description;
+            Quantity = quantity;
+            Currency = currency;
+            ConditionId = conditionId;
+            Barcode = barcode;
+            DateSold = dateSold;
+        }
 
-		public string Title { get; set; } = string.Empty;
-		public int CategoryId { get; set; }
-		public decimal PurchasePrice { get; set; }
-		public decimal SellPrice { get; set; }
-		public string Description { get; set; } = string.Empty;
-		public int Quantity { get; set; }
-		public string Currency { get; set; } = string.Empty;
-		public int ConditionId { get; set; }
-		public int Barcode { get; set; }
-		public DateOnly DateSold { get; set; }	
-	}
+        public string Title { get; set; } = string.Empty;
+        public int CategoryId { get; set; }
+        public decimal PurchasePrice { get; set; }
+        public decimal SellPrice { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public int Quantity { get; set; }
+        public string Currency { get; set; } = string.Empty;
+        public int ConditionId { get; set; }
+        public string Barcode { get; set; }
+        public string DateSold { get; set; }
+    }
 }
