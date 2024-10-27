4a0c8a5e435f3f0b814908a34ca7d4e3836bae22
Jalen Ashanti <jalenmshaw@gmail.com>
2024-10-27 15:14:54 -0400

    Client Layer: Created Client for DonutChartViewModel

---
+++ FlipBuddyWebApplication.Client/ViewModels/Home/DonutChart/DonutChartViewModelClient.cs
@@ -0,0 +1,16 @@
+﻿using FlipBuddyWebApplication.Persistence.Repositories;
+
+namespace FlipBuddyWebApplication.Client.ViewModels.Home.DonutChart
+{
+    public class DonutChartViewModelClient
+    {
+        public readonly ProductRepository _productRepository;
+        public readonly CategoryRepository _categoryRepository;
+       
+        public DonutChartViewModelClient(ProductRepository productRepository, CategoryRepository categoryRepository) 
+        {
+            _productRepository = productRepository;
+            _categoryRepository = categoryRepository;
+        }
+    }
+}
