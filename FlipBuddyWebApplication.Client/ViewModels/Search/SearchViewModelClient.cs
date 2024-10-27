8f5427eddef0551ac107071502d41bc23bc43ff2
Jalen Ashanti <jalenmshaw@gmail.com>
2024-10-27 15:14:19 -0400

    Client Layer: Created Client for SearchPage

---
+++ FlipBuddyWebApplication.Client/ViewModels/Search/SearchViewModelClient.cs
@@ -0,0 +1,15 @@
+﻿using FlipBuddyWebApplication.Client.ViewModels.Abstractions;
+using FlipBuddyWebApplication.Persistence.Repositories;
+
+namespace FlipBuddyWebApplication.Client.ViewModels.Search
+{
+	public class SearchViewModelClient : BaseClient
+	{
+		public readonly CategoryRepository _categoryRepository;
+
+		public SearchViewModelClient(CategoryRepository categoryRepository) 
+		{ 
+			_categoryRepository = categoryRepository; 
+		}
+	}
+}
