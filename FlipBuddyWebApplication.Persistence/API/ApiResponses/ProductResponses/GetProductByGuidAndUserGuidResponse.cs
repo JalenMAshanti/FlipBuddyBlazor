b67209b84d8b5363a8be83303bcbd1a9fb508b03
Jalen Ashanti <jalenmshaw@gmail.com>
2024-10-27 15:31:31 -0400

    Persistence Layer: Create response model for GetProductByGuidAndUserGuid endpoint

---
+++ FlipBuddyWebApplication.Persistence/API/ApiResponses/ProductResponses/GetProductByGuidAndUserGuidResponse.cs
@@ -0,0 +1,12 @@
+﻿using FlipBuddyWebApplication.Domain.Models;
+
+namespace FlipBuddyWebApplication.Persistence.API.ApiResponses.ProductResponses
+{
+
+    public class GetProductByGuidAndUserGuidResponse
+    {
+        public ProductResponse product { get; set; }
+
+        public Product AsDomainProduct() => new(product.guid, product.title, product.categoryId, product.purchasedPrice, product.sellPrice, product.description, product.quantity, product.currency, product.conditionId, product.barCode, product.dateSold);
+    }
+}
