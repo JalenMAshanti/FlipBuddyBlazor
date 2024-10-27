58345c3ec0b3a8e3846d05e4c6e3bb2c5e02cda0
Jalen Ashanti <jalenmshaw@gmail.com>
2024-10-27 15:05:05 -0400

    Domain Layer: Add DataConverter and MonthHelper

---
+++ FlipBuddyWebApplication.Domain/Helpers/DateConverters.cs
@@ -0,0 +1,16 @@
+﻿namespace FlipBuddyWebApplication.Domain.Helpers
+{
+    public static class DateConverters
+    {
+        public static DateOnly ConvertStringToDateOnly(string dateString)
+        {
+            var withTime = DateTime.Parse(dateString);
+            return DateOnly.FromDateTime(withTime);
+        }
+
+        public static string ConvertDateOnlyToString(DateOnly dateOnly)
+        {
+            return dateOnly.ToString("yyyy-M-d");
+        }
+    }
+}
---
+++ FlipBuddyWebApplication.Domain/Helpers/MonthHelper.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+
+namespace FlipBuddyWebApplication.Domain.Helpers
+{
+    public class MonthHelper
+    {
+        public static List<(int Month, int Year, string MonthName)> GetLastEightMonthsWithYear()
+        {
+            List<(int Month, int Year, string MonthName)> months = new List<(int, int, string)>();
+            int currentMonth = DateTime.Now.Month;
+            int currentYear = DateTime.Now.Year;
+
+            // Loop through the last 8 months, including the current month
+            for (int i = 7; i >= 0; i--)
+            {
+                // Calculate the month and year
+                int month = (currentMonth - i + 11) % 12 + 1;
+                int year = currentYear - (currentMonth - i <= 0 ? 1 : 0);
+
+                // Get the abbreviated name of the month
+                string monthName = CultureInfo.CurrentCulture.DateTimeFormat.GetAbbreviatedMonthName(month);
+
+                months.Add((month, year, monthName));
+            }
+
+            return months;
+        }
+    }
+}
