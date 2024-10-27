da740077f69b317e7717a9c14fd1e9b9d751f929
Jalen Ashanti <jalenmshaw@gmail.com>
2024-10-27 15:18:11 -0400

    Domain Layer: Created class to hold data and avoid redundant API calls to backend

---
+++ FlipBuddyWebApplication.Domain/CachedData/CachedAppData.cs
@@ -0,0 +1,13 @@
+﻿using FlipBuddyWebApplication.Domain.Models;
+
+namespace FlipBuddyWebApplication.Domain.CachedData
+{
+    public static class CachedAppData
+    {
+        // used for data that doesnt need to keep making calls to api
+
+        public static List<Category>? AllCategoies { get; set; } = new List<Category>();
+
+        public static List<Condition>? AllConditions { get; set; } = new List<Condition>();
+    }
+}
