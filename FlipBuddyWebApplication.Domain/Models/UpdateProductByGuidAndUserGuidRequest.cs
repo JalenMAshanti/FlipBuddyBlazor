0bb8280eaca2ec1d6903bcc21e817dec459715f7
Jalen Ashanti <jalenmshaw@gmail.com>
2024-10-27 15:27:20 -0400

    cleaned using statements

--- FlipBuddyWebApplication.Domain/Models/UpdateProductByGuidAndUserGuidRequest.cs
+++ FlipBuddyWebApplication.Domain/Models/UpdateProductByGuidAndUserGuidRequest.cs
@@ -1,46 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace FlipBuddyWebApplication.Domain.Models
 {
-    public class UpdateProductByGuidAndUserGuidRequest
-    {
-        public Guid Guid { get; set; }
+	public class UpdateProductByGuidAndUserGuidRequest
+	{
+		public Guid Guid { get; set; }
 
-        public Guid UserGuid { get; set; } 
+		public Guid UserGuid { get; set; }
 
-        [Required]
-        public string Title { get; set; } = string.Empty;
+		[Required]
+		public string Title { get; set; } = string.Empty;
 
-        [Required]
-        public int CategoryId { get; set; }
+		[Required]
+		public int CategoryId { get; set; }
 
-        [Required]
-        public decimal PurchasedPrice { get; set; }
+		[Required]
+		public decimal PurchasedPrice { get; set; }
 
-        [Required]
-        public decimal SellPrice { get; set; }
+		[Required]
+		public decimal SellPrice { get; set; }
 
-        [Required]
-        [StringLength(300)]
-        public string Description { get; set; } = string.Empty;
+		[Required]
+		[StringLength(300)]
+		public string Description { get; set; } = string.Empty;
 
-        [Required]
-        [Range(1, 100)]
-        public int Quantity { get; set; } = 1;
+		[Required]
+		[Range(1, 100)]
+		public int Quantity { get; set; } = 1;
 
-        [Required]
-        public string Currency { get; set; } = string.Empty;
+		[Required]
+		public string Currency { get; set; } = string.Empty;
 
-        [Required]
-        public int ConditionId { get; set; }
+		[Required]
+		public int ConditionId { get; set; }
 
-        public string BarCode { get; set; } = string.Empty;
+		public string BarCode { get; set; } = string.Empty;
 
-        public DateOnly? DateSold { get; set; } 
-    }
+		public DateOnly? DateSold { get; set; }
+	}
 }
