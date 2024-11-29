95f05846d311b165d6e314f7cb18b50e9395e57c
Jalen Ashanti <jalenmshaw@gmail.com>
2024-11-29 01:27:07 -0500

    Persistence Layer: Created EbayRepository

---
+++ FlipBuddyWebApplication.Persistence/Repositories/EbayRepository.cs
@@ -0,0 +1,12 @@
+﻿using FlipBuddyWebApplication.Persistence.API.Abstractions;
+using FlipBuddyWebApplication.Persistence.Factories;
+
+namespace FlipBuddyWebApplication.Persistence.Repositories
+{
+    public class EbayRepository : EbayAPIService
+    {
+        public EbayRepository(ClientFactory clientFactory) : base(clientFactory)
+        {
+        }
+    }
+}
