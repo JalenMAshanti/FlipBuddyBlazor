92845866bd52c1ff74f921c5876de4b93318909a
Jalen Ashanti <jalenmshaw@gmail.com>
2024-12-09 23:54:36 -0500

    Client Layer: Created SettingsClient for Settings Component

---
+++ FlipBuddyWebApplication.Client/ViewModels/Settings/SettingsClient.cs
@@ -0,0 +1,13 @@
+﻿using FlipBuddyWebApplication.Persistence.Repositories;
+
+namespace FlipBuddyWebApplication.Client.ViewModels.Settings
+{
+    public class SettingsClient
+    {
+        public readonly UserRepository _userRepository;
+        public SettingsClient(UserRepository userRepository) 
+        {
+            _userRepository = userRepository;
+        }       
+    }
+}
